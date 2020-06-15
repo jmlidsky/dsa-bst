322c567f04f9a4c08b6d3196a83ff3d3c5a806c2
Jill Lidsky <jilllidsky15@gmail.com>
2020-06-15 15:49:09 -0400

    added drills

--- drills.js
+++ drills.js
@@ -1,6 +1,6 @@
 const BinarySearchTree = require('./bst')
 
-function main() {
+function main1() {
     const BST = new BinarySearchTree()
 
     BST.insert(3);
@@ -13,6 +13,53 @@ function main() {
     BST.insert(7);
 
     console.log(BST)
+
 }
 
-main()
+main1()
+
+function main2() {
+    const BST = new BinarySearchTree()
+
+    BST.insert('E');
+    BST.insert('A');
+    BST.insert('S');
+    BST.insert('Y');
+    BST.insert('Q');
+    BST.insert('U');
+    BST.insert('E');
+    BST.insert('S');
+    BST.insert('T');
+    BST.insert('I');
+    BST.insert('O');
+    BST.insert('N');
+
+    console.log(BST)
+}
+
+main2()
+
+// 4. What does this program do?
+// Without running this code in your code editor, explain what the following program does. Show with an example the result of executing this program. What is the runtime of this algorithm?
+
+function tree(t){
+    if(!t){
+        return 0;
+    }
+    return tree(t.left) + t.value + tree(t.right)
+}
+
+// 5. Height of a BST
+// Write an algorithm to find the height of a binary search tree. What is the time complexity of your algorithm?
+
+// 6. Is it a BST?
+// Write an algorithm to check whether an arbitrary binary tree is a binary search tree, assuming the tree does not contain duplicates.
+
+// 7. 3rd largest node
+// Write an algorithm to find the 3rd largest node in a binary search tree.
+
+// 8. Balanced BST
+// Write an algorithm that checks if a BST is balanced (i.e., a tree where no 2 leaves differ in distance from the root by more than 1).
+
+// 9. Are they the same BSTs?
+// You are given two arrays which represent two sequences of keys that are used to create two binary search trees. Write a program that will tell whether the two BSTs will be identical or not without actually constructing the tree. You may use another data structure such as an array or a linked list but don't construct the BST. What is the time complexity of your algorithm? E.g., 3, 5, 4, 6, 1, 0, 2 and 3, 1, 5, 2, 4, 6, 0 are two sequences of arrays but will create the exact same BSTs and your program should return true.
